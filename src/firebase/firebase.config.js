583254b605b2bee1b0590b6927fe8d0f106bc7f0
shakilwebdev <shakilwebdeveloper1@gmail.com>
2025-11-26 14:53:33 +0600

    .env file added firebaseConfig file secure done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,22 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyD2Vy1g-B9jMe4VFnv8Oj_WJl-UsZmhK2A",
   authDomain: "ai-model-8601b.firebaseapp.com",
   projectId: "ai-model-8601b",
   storageBucket: "ai-model-8601b.firebasestorage.app",
   messagingSenderId: "264436805663",
   appId: "1:264436805663:web:59755398cc2acd764a652a",
+};
+ */
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
